e70ee7c1c89a5df124ac64f81c7960e3bd281c1d
cccl33333 <47235562+cccl33333@users.noreply.github.com>
2019-02-01 15:45:55 +0800

    Add files via upload

---
+++ blackmeet001/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace blackmeet001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (var i = 0; i <= 100; i++)
+            
+                Console.WriteLine(i.ToString().Replace("3", "A").Replace("5", "B").Replace("9", "C").Replace("0", "D"));
+            
+            Console.ReadLine();
+        }
+    }
+}
+// if (65 <= number && 90 >= number)
+//                            System.Text.ASCIIEncoding asciiEncoding = new System.Text.ASCIIEncoding();
+//byte[] btNumber = new byte[] { (byte)number };
+//                    return asciiEncoding.GetString(btNumber);
